2a09b0f85b4aa0789490d70a9ca6153a96a37101
Richard Shea <rshea@thecubagroup.com>
2021-11-12 10:05:51 +1300

    Placeholder page to serve JSON

--- ts-kv-tester/src/handler.ts
+++ ts-kv-tester/src/handler.ts
@@ -1,5 +1,13 @@
+import * as UrlUtils from '@dashlane/simple-url-parser';
 import { handleRequestForIndexPage } from './handlerIndexPage'
+import { handleRequestForJSON } from './handlerJSONPage'
 
 export async function handleRequest(request: Request): Promise<Response> {
-    return handleRequestForIndexPage(request);
+    var filePath = UrlUtils.extractFilepathFromUrl(request.url);
+    switch ( filePath ) {
+        case "/":
+            return handleRequestForIndexPage(request);
+        default:
+            return handleRequestForJSON(request);
+    }
 }
