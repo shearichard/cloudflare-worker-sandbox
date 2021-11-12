da2aa647652219ae37a75818d0abb49110866b96
Richard Shea <rshea@thecubagroup.com>
2021-11-12 13:19:54 +1300

    Now saving values to KV

--- ts-kv-tester/src/handlerJSONPage.ts
+++ ts-kv-tester/src/handlerJSONPage.ts
@@ -1,13 +1,22 @@
 
+declare global {
+      const SHEARICHARD_TEST_0: KVNamespace;
+}
 export async function handleRequestForJSON(request: Request): Promise<Response> {
-    //
-    const data = {
-        hello: "world",
-        age:44,
-        address: "44, Acacia Avenue"
+    let datetimeiso = new Date().toISOString()
+    let model_name = Math.random().toString().substr(2, 8);
+    const defaultData = { 
+        todos: [
+            {
+                id: datetimeiso,
+                name: model_name,
+                completed: false
+            }
+        ] 
     }
+    SHEARICHARD_TEST_0.put(datetimeiso, JSON.stringify(defaultData))
     //
-    const json = JSON.stringify(data, null, 2)
+    const json = JSON.stringify(defaultData, null, 2)
     //
     return new Response(json, {
         headers: {
